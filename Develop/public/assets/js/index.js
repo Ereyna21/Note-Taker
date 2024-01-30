5a6e0f18464238edd6ffb1d9794e378c68d2c983
Ereyna21 <153467127+Ereyna21@users.noreply.github.com>
2024-01-29 21:54:33 -0600

    index.html, root route, dynamic port, gitignore, dependencies in package.json

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -4,15 +4,17 @@ let noteText;
 let saveNoteBtn;
 let newNoteBtn;
 let noteList;
+let clearBtn;
 
 if (window.location.pathname === '/notes') {
   noteForm = document.querySelector('.note-form');
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
   saveNoteBtn = document.querySelector('.save-note');
-  newNoteBtn = document.querySelector('.new-note');
+  newNoteBtn = document.querySelector('.icons .new-note');
   clearBtn = document.querySelector('.clear-btn');
   noteList = document.querySelectorAll('.list-container .list-group');
+ 
 }
 
 // Show an element
@@ -20,24 +22,24 @@ const show = (elem) => {
   elem.style.display = 'inline';
 };
 
-// Hide an element
 const hide = (elem) => {
   elem.style.display = 'none';
 };
+
 
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
 
 const getNotes = () =>
-  fetch('/api/notes', {
+  fetch('/notes', {
     method: 'GET',
     headers: {
-      'Content-Type': 'application/json'
-    }
-  });
+      'Content-Type': 'application/json',
+    },
+  }).catch(error => console.error('Error fetching notes:', error));
 
-const saveNote = (note) =>
-  fetch('/api/notes', {
+const postNote = (note) =>
+  fetch('/notes', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -77,7 +79,7 @@ const handleNoteSave = () => {
     title: noteTitle.value,
     text: noteText.value
   };
-  saveNote(newNote).then(() => {
+  postNote(newNote).then(() => {
     getAndRenderNotes();
     renderActiveNote();
   });
---
+++ Develop/server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const uuid = require('./helpers/uuid');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET request for notes
+app.get('/api/notes', (req, res) => {
+  // Read notes from the file and send them as a response
+  fs.readFile('./db/notes.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json(`${req.method} Error reading notes`);
+    } else {
+      const parsedNotes = JSON.parse(data);
+      res.status(200).json(`${req.method} parsedNotes`);
+    }
+  });
+});
+
+// Route for notes.html
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+// POST request to add a note
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+
+    fs.readFile('./db/notes.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json('Error reading notes');
+      } else {
+        const parsedNotes = JSON.parse(data);
+        parsedNotes.push(newNote);
+
+        fs.writeFile(
+          './db/notes.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) => {
+            if (writeErr) {
+              console.error(writeErr);
+              res.status(500).json('Error writing notes');
+            } else {
+              console.info('Successfully updated notes!');
+              res.status(201).json(newNote);
+            }
+          }
+        );
+      }
+    });
+  } else {
+    res.status(400).json('Bad request. Title and text are required');
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
+
